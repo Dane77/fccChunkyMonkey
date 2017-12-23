5651ef07aa7732e9d782a6298428b5be67c890bf
Dane77 <bolesd77@gmail.com>
2017-12-22 19:51:20 -0500

    Create fccChunkyMonkey.js

---
+++ fccChunkyMonkey.js
@@ -0,0 +1,10 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  var newArray = [];
+  var i = 0;
+  while (i < arr.length){
+    newArray.push(arr.slice(i,i+size));
+    i += size;
+  }
+  return newArray;
+}
